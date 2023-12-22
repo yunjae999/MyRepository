4c7da6d3f7728450e4b40c97711897dbd9ce55b8
BLUE <BLUE@1.232.217.107>
2023-12-22 13:55:55 +0900

    Change in master branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -3,6 +3,7 @@ package mygit;
 public class YourClass {
 	public void sayHello(String toWhom) {
 		System.out.println("Hello! " + toWhom);
+		System.out.println("Your changed in master branch");
 	}
 
 }
