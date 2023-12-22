13c30d1061c6a5020f990f944c08bb29012fbf5a
BLUE <BLUE@1.232.217.107>
2023-12-22 13:53:13 +0900

    First commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 		}
 }
